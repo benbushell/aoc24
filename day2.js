a60425ea73c0afcd7c691a10537aa7c8b96c1aaf
benbushell <82777448+benbushell@users.noreply.github.com>
2024-12-04 17:24:40 +0000

    split out puzzle text for day 2

--- day2.js
+++ day2.js
@@ -1,6 +1,35 @@
-function solution() {
-    console.log('Hello world')
+const fs = require('fs');
 
+fs.readFile('./day2_puzzle.txt', (err, data) => {
+    if (err) throw err;
+    console.log(data.toString())
+    doSolution(data.toString())
+
+})
+
+
+
+
+function doSolution(puzzle) {
+    console.log(puzzle)
+    let temp = puzzle.split("\n");
+    let data = [];
+    for (let layer of temp) {
+        data.push(layer.split(" "));
+    }
+    console.log(data);
+
+    for (let layer of data) {
+        let safe = true;
+        for (let i = 0; i < layer.length - 1; i++) {
+            let curr = layer[i];
+            let next = layer[i + 1];
+            if (Math.abs(curr - next) >= 1 && Math.abs(curr - next) <= 3) {
+                console.log(i);
+            } else {
+                safe = false;
+            }
+        }
+        console.log(safe);
+    }
 }
-
-solution()
